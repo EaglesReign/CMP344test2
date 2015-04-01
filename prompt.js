4fab83792c62bf972e5ef7d46dd159d582c7d5f6
Phillip <phv9495@saintjoe.edu>
2015-04-01 13:41:20 -0400

    Add the code for problem 2 of Javascript 101

---
+++ prompt.js
@@ -0,0 +1,15 @@
+//Phil Hofer
+//prompt.js
+//CMP344 Exam 2 Practicum
+
+//Problem 3 of Javascript 101
+//This program prompts the user for a string and then an integer and will
+//print them back to the user.
+
+print("Enter a string.");
+var input1 = readline();
+print("Enter an integer.");
+var input2 = readline();
+
+print("You entered '" + input1 + "' for your string input, and '" + 
+		input2 + "' for your integer.");
