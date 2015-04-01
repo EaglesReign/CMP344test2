37a4cc9931fecbac039988a022dd431f66013a8f
Phillip <phv9495@saintjoe.edu>
2015-04-01 13:32:24 -0400

    Add code for problem 1 of Javascript 101

---
+++ array.js
@@ -0,0 +1,18 @@
+//Phil Hofer
+//CMP344 Exam 2 Practicum
+//4/1/15
+//array.js
+
+//Problem 1 under Javascript 101
+
+function array() {
+  var arr = [];
+  var min = 10;
+  var max = 200;
+  for (var i = 0; i < 20; i++) {
+    arr[i] = Math.floor(Math.random() * max + min);
+    print(arr[i]);
+  }
+}
+
+array();
